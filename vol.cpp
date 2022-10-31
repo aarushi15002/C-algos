8ad09e85cde696a998f9b900000483fb873c0b31
Virendra_kumar <117063481+VRNDRYDV@users.noreply.github.com>
2022-10-31 12:24:26 +0530

    Create vol.cpp

---
+++ vol.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*int rad1;
+float volsp;
+cout<<"enter the value of radius:";
+cin>>rad1;
+volsp=(4*3.14*rad1*rad1*rad1)/3;
+cout<<"Volume of sphere is:"<<volsp<<endl;
+
+
+return 0;
+*/
+int rad1,height;
+float volc;
+cout<<"enter the radius of cylinder";
+cin>>rad1;
+cout<<"enter the height of cylinder";
+cin>>height;
+volc=3.14*rad1*rad1*height;
+cout<<"The volume of cylinder is:"<<volc<<endl;
+return 0;
+
+}
